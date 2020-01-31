fe27799c39ca0f14af7e9799e02b1a437033890b
gofogo2 <promoted.go@gmail.com>
2020-01-31 15:22:32 +0900

    Updates

---
+++ precache-manifest.3772252d769cbd39475ed03a9a90d190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6455a2c8cad3a4b70cfaa1afcc129040",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "e206bb3e0115909c65ed",
+    "url": "/movie_app_2020/static/css/main.5dd66e2a.chunk.css"
+  },
+  {
+    "revision": "9966fbb9ece634a56ad4",
+    "url": "/movie_app_2020/static/js/2.44bdb5fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.44bdb5fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e206bb3e0115909c65ed",
+    "url": "/movie_app_2020/static/js/main.c0a594d9.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
